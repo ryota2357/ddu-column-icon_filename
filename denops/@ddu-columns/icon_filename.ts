7b599144ea4c142059ac9327a775218a2ffc9493
2357 <ryoooo2357g@gmail.com>
2022-11-07 19:21:05 +0900

    feat!: change color palette name
    !normal -> !default

--- denops/@ddu-columns/icon_filename.ts
+++ denops/@ddu-columns/icon_filename.ts
@@ -101,7 +101,7 @@ export class Column extends BaseColumn<Params> {
       return col.startsWith("!")
         ? args.columnParams.colors[col.slice(1)] ??
           defaultColors.get(col.slice(1)) ??
-          defaultColors.get("normal")!
+          defaultColors.get("default")!
         : col;
     })();
     highlights.push({
@@ -128,7 +128,7 @@ export class Column extends BaseColumn<Params> {
       span: 1,
       padding: 1,
       iconWidth: 1,
-      defaultIcon: { icon: " ", color: "!normal" },
+      defaultIcon: { icon: " ", color: "!default" },
       linkIcon: { icon: "", color: "#808080" },
       useLinkIcon: "always",
       customSpecialIcons: {},
@@ -238,7 +238,7 @@ export class Column extends BaseColumn<Params> {
 }
 
 const defaultColors = new Map<string, string>([
-  ["normal", "Normal"],
+  ["default", "Normal"],
   ["aqua", "#3AFFDB"],
   ["beige", "#F5C06F"],
   ["blue", "#689FB6"],
@@ -258,7 +258,7 @@ const defaultColors = new Map<string, string>([
 
 // for preventing typo
 const palette = {
-  normal: "!normal",
+  default: "!default",
   aqua: "!aqua",
   beige: "!beige",
   blue: "!blue",
@@ -283,17 +283,17 @@ const folderIcons: Record<"expand" | "collaps", IconData> = {
 
 // deno-fmt-ignore-start
 const specialIcons = new Map<string, IconData>([                                               // nerd font class name
-  [".ds_store",          { icon: "", hl_group: "sp_ds_store",    color: palette.normal     }], // nf-dev-aptana
+  [".ds_store",          { icon: "", hl_group: "sp_ds_store",    color: palette.default    }], // nf-dev-aptana
   [".gitignore",         { icon: "", hl_group: "sp_gitignore",   color: palette.darkOrange }], // nf-dev-git
-  [".gitconfig",         { icon: "", hl_group: "sp_gitconfig",   color: palette.normal     }], // nf-dev-aptana
-  [".gitlab-ci.yml",     { icon: "", hl_group: "sp_gitlab_ci",   color: palette.normal     }], // nf-fa-gitlab
-  ["config.ru",          { icon: "", hl_group: "sp_config_ru",   color: palette.normal     }], // nf-dev-ruby
+  [".gitconfig",         { icon: "", hl_group: "sp_gitconfig",   color: palette.default    }], // nf-dev-aptana
+  [".gitlab-ci.yml",     { icon: "", hl_group: "sp_gitlab_ci",   color: palette.default    }], // nf-fa-gitlab
+  ["config.ru",          { icon: "", hl_group: "sp_config_ru",   color: palette.default    }], // nf-dev-ruby
   ["dockerfile",         { icon: "", hl_group: "sp_license",     color: palette.blue       }], // nf-dev-docker
   ["docker-compose.yml", { icon: "", hl_group: "sp_license",     color: palette.yellow     }], // nf-dev-docker
   ["favicon.ico",        { icon: "", hl_group: "sp_favicon",     color: palette.yellow     }], // nf-seti-favicon
-  ["makefile",           { icon: "", hl_group: "sp_gitconfig",   color: palette.normal     }], // nf-dev-aptana
-  ["license",            { icon: "", hl_group: "sp_license",     color: palette.normal     }], // nf-seti-license
-  ["license.txt",        { icon: "", hl_group: "sp_license",     color: palette.normal     }], // nf-seti-license
+  ["makefile",           { icon: "", hl_group: "sp_gitconfig",   color: palette.default    }], // nf-dev-aptana
+  ["license",            { icon: "", hl_group: "sp_license",     color: palette.default    }], // nf-seti-license
+  ["license.txt",        { icon: "", hl_group: "sp_license",     color: palette.default    }], // nf-seti-license
   ["readme",             { icon: "", hl_group: "sp_readme",      color: palette.yellow     }], // nf-seti-markdown
   ["readme.md",          { icon: "", hl_group: "sp_readme",      color: palette.yellow     }], // nf-seti-markdown
   ["changelog",          { icon: "", hl_group: "sp_changelog",   color: palette.green      }], // nf-fa-history
@@ -308,26 +308,26 @@ const specialIcons = new Map<string, IconData>([                                
 // deno-fmt-ignore-start
 const fileIcons = new Map<string, IconData>([                                    // nerd font class name
   ["ai",     { icon: "", hl_group: "file_ai",     color: palette.darkOrange  }], // nf-dev-illustrator
-  ["awk",    { icon: "", hl_group: "file_awk",    color: palette.normal      }], // nf-dev-terminal
-  ["bash",   { icon: "", hl_group: "file_bash",   color: palette.normal      }], // nf-dev-terminal
-  ["bat",    { icon: "", hl_group: "file_bat",    color: palette.normal      }], // nf-dev-aptana
+  ["awk",    { icon: "", hl_group: "file_awk",    color: palette.default     }], // nf-dev-terminal
+  ["bash",   { icon: "", hl_group: "file_bash",   color: palette.default     }], // nf-dev-terminal
+  ["bat",    { icon: "", hl_group: "file_bat",    color: palette.default     }], // nf-dev-aptana
   ["bmp",    { icon: "", hl_group: "file_bmp",    color: palette.aqua        }], // nf-fa-file_image_o
   ["c",      { icon: "", hl_group: "file_c",      color: palette.blue        }], // nf-custom-c
   ["clj",    { icon: "", hl_group: "file_clj",    color: palette.green       }], // nf-dev-clojure
   ["cljc",   { icon: "", hl_group: "file_cljc",   color: palette.green       }], // nf-dev-clojure
   ["cljs",   { icon: "", hl_group: "file_cljs",   color: palette.green       }], // nf-dev-clojure_alt
   ["coffee", { icon: "", hl_group: "file_coffee", color: palette.brown       }], // nf-dev-coffeescript
-  ["conf",   { icon: "", hl_group: "file_conf",   color: palette.normal      }], // nf-dev-aptana
+  ["conf",   { icon: "", hl_group: "file_conf",   color: palette.default     }], // nf-dev-aptana
   ["cpp",    { icon: "", hl_group: "file_cpp",    color: palette.blue        }], // nf-custom-cpp
   ["cs",     { icon: "", hl_group: "file_cs",     color: palette.blue        }], // nf-mdi-language_csharp
-  ["csh",    { icon: "", hl_group: "file_csh",    color: palette.normal      }], // nf-dev-terminal
+  ["csh",    { icon: "", hl_group: "file_csh",    color: palette.default     }], // nf-dev-terminal
   ["css",    { icon: "", hl_group: "file_css",    color: palette.blue        }], // nf-dev-css3
   ["d",      { icon: "", hl_group: "file_d",      color: palette.red         }], // nf-dev-dlangd
   ["db",     { icon: "", hl_group: "file_db",     color: palette.blue        }], // nf-fa-database
   ["dart",   { icon: "", hl_group: "file_dart",   color: palette.blue        }], // nf-dev-dart
   ["doc",    { icon: "", hl_group: "file_doc",    color: palette.darkBlue    }], // nf-fa-file_word_o
   ["docx",   { icon: "", hl_group: "file_docx",   color: palette.darkBlue    }], // nf-fa-file_word_o
-  ["elm",    { icon: "", hl_group: "file_elm",    color: palette.normal      }], // nf-dev-dart
+  ["elm",    { icon: "", hl_group: "file_elm",    color: palette.default     }], // nf-dev-dart
   ["ex",     { icon: "", hl_group: "file_ex",     color: palette.lightPurple }], // nf-custom-elixir
   ["exs",    { icon: "", hl_group: "file_exs",    color: palette.lightPurple }], // nf-custom-elixir
   ["fish",   { icon: "", hl_group: "file_fish",   color: palette.green       }], // nf-dev-terminal
@@ -335,9 +335,9 @@ const fileIcons = new Map<string, IconData>([                                   
   ["fsx",    { icon: "", hl_group: "file_fsx",    color: palette.blue        }], // nf-dev-fsharp
   ["gif",    { icon: "", hl_group: "file_gif",    color: palette.aqua        }], // nf-fa-file_image_o
   ["go",     { icon: "", hl_group: "file_go",     color: palette.beige       }], // nf-dev-go
-  ["gz",     { icon: "", hl_group: "file_gz",     color: palette.normal      }], // nf-oct-file_zip
-  ["h",      { icon: "", hl_group: "file_h",      color: palette.normal      }], // nf-fa-h_square
-  ["hpp",    { icon: "", hl_group: "file_hpp",    color: palette.normal      }], // nf-fa-h_square
+  ["gz",     { icon: "", hl_group: "file_gz",     color: palette.default     }], // nf-oct-file_zip
+  ["h",      { icon: "", hl_group: "file_h",      color: palette.default     }], // nf-fa-h_square
+  ["hpp",    { icon: "", hl_group: "file_hpp",    color: palette.default     }], // nf-fa-h_square
   ["hs",     { icon: "", hl_group: "file_hs",     color: palette.beige       }], // nf-dev-haskell
   ["html",   { icon: "", hl_group: "file_html",   color: palette.darkOrange  }], // nf-dev-html5
   ["ico",    { icon: "", hl_group: "file_ico",    color: palette.aqua        }], // nf-fa-file_image_o
@@ -359,7 +359,7 @@ const fileIcons = new Map<string, IconData>([                                   
   ["pl",     { icon: "", hl_group: "file_pl",     color: palette.blue        }], // nf-dev-perl
   ["pm",     { icon: "", hl_group: "file_pm",     color: palette.blue        }], // nf-dev-perl
   ["png",    { icon: "", hl_group: "file_png",    color: palette.aqua        }], // nf-fa-file_image_o
-  ["pp",     { icon: "", hl_group: "file_pp",     color: palette.normal      }], // nf-oct-beaker
+  ["pp",     { icon: "", hl_group: "file_pp",     color: palette.default     }], // nf-oct-beaker
   ["ppt",    { icon: "", hl_group: "file_ppt",    color: palette.orange      }], // nf-fa-file_powerpoint_o
   ["pptx",   { icon: "", hl_group: "file_pptx",   color: palette.orange      }], // nf-fa-file_powerpoint_o
   ["psd",    { icon: "", hl_group: "file_psd",    color: palette.darkBlue    }], // nf-dev-photoshop
@@ -369,7 +369,7 @@ const fileIcons = new Map<string, IconData>([                                   
   ["rmd",    { icon: "", hl_group: "file_rmd",    color: palette.blue        }], // nf-dev-markdown
   ["rs",     { icon: "", hl_group: "file_rs",     color: palette.red         }], // nf-dev-rust
   ["rss",    { icon: "", hl_group: "file_rss",    color: palette.darkOrange  }], // nf-fa-rss
-  ["sass",   { icon: "", hl_group: "file_sass",   color: palette.normal      }], // nf-dev-sass
+  ["sass",   { icon: "", hl_group: "file_sass",   color: palette.default     }], // nf-dev-sass
   ["scala",  { icon: "", hl_group: "file_scala",  color: palette.red         }], // nf-dev-scala
   ["scss",   { icon: "", hl_group: "file_scss",   color: palette.pink        }], // nf-dev-sass
   ["sh",     { icon: "", hl_group: "file_sh",     color: palette.lightPurple }], // nf-dev-terminal
@@ -377,19 +377,19 @@ const fileIcons = new Map<string, IconData>([                                   
   ["sln",    { icon: "", hl_group: "file_sln",    color: palette.purple      }], // nf-dev-visualstudio
   ["styl",   { icon: "", hl_group: "file_styl",   color: palette.green       }], // nf-dev-stylus
   ["swift",  { icon: "", hl_group: "file_swift",  color: palette.orange      }], // nf-dev-swift
-  ["tex",    { icon: "", hl_group: "file_tex",    color: palette.normal      }], // nf-mdi-text_shadow
-  ["toml",   { icon: "", hl_group: "file_toml",   color: palette.normal      }], // nf-dev-aptana
+  ["tex",    { icon: "", hl_group: "file_tex",    color: palette.default     }], // nf-mdi-text_shadow
+  ["toml",   { icon: "", hl_group: "file_toml",   color: palette.default     }], // nf-dev-aptana
   ["ts",     { icon: "", hl_group: "file_ts",     color: palette.blue        }], // nf-seti-typescript
   ["tsx",    { icon: "", hl_group: "file_tsx",    color: palette.blue        }], // nf-dev-react
-  ["txt",    { icon: "", hl_group: "file_txt",    color: palette.normal      }], // nf-seti-text
+  ["txt",    { icon: "", hl_group: "file_txt",    color: palette.default     }], // nf-seti-text
   ["vim",    { icon: "", hl_group: "file_vim",    color: palette.green       }], // nf-dev-vim
   ["vue",    { icon: "﵂", hl_group: "file_vue",    color: palette.green       }], // nf-mdi-vuejs
   ["webp",   { icon: "", hl_group: "file_webp",   color: palette.aqua        }], // nf-fa-file_image_o
   ["xls",    { icon: "", hl_group: "file_xls",    color: palette.lightGreen  }], // nf-fa-file_excel_o
   ["xlsx",   { icon: "", hl_group: "file_xlsx",   color: palette.lightGreen  }], // nf-fa-file_excel_o
-  ["yaml",   { icon: "", hl_group: "file_yaml",   color: palette.normal      }], // nf-dev-aptana
-  ["yml",    { icon: "", hl_group: "file_yml",    color: palette.normal      }], // nf-dev-aptana
-  ["zip",    { icon: "", hl_group: "file_zip",    color: palette.normal      }], // nf-oct-file_zip
-  ["zsh",    { icon: "", hl_group: "file_zsh",    color: palette.normal      }], // nf-dev-terminal
+  ["yaml",   { icon: "", hl_group: "file_yaml",   color: palette.default     }], // nf-dev-aptana
+  ["yml",    { icon: "", hl_group: "file_yml",    color: palette.default     }], // nf-dev-aptana
+  ["zip",    { icon: "", hl_group: "file_zip",    color: palette.default     }], // nf-oct-file_zip
+  ["zsh",    { icon: "", hl_group: "file_zsh",    color: palette.default     }], // nf-dev-terminal
 ]);
 // deno-fmt-ignore-end
