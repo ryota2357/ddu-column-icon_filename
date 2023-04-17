dbf729b37c71137cbcb63e74581ef75d66d5e8df
2357 <61523777+ryota2357@users.noreply.github.com>
2023-04-17 21:06:29 +0900

    update deps

--- denops/@ddu-columns/icon_filename.ts
+++ denops/@ddu-columns/icon_filename.ts
@@ -2,15 +2,15 @@ import {
   BaseColumn,
   DduItem,
   ItemHighlight,
-} from "https://deno.land/x/ddu_vim@v2.6.0/types.ts";
-import { GetTextResult } from "https://deno.land/x/ddu_vim@v2.6.0/base/column.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.6.0/deps.ts";
-import { ambiwidth } from "https://deno.land/x/denops_std@v4.1.0/option/mod.ts";
+} from "https://deno.land/x/ddu_vim@v2.8.0/types.ts";
+import { GetTextResult } from "https://deno.land/x/ddu_vim@v2.8.0/base/column.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.8.0/deps.ts";
+import { ambiwidth } from "https://deno.land/x/denops_std@v4.1.5/option/mod.ts";
 import {
   basename,
   extname,
   relative,
-} from "https://deno.land/std@0.180.0/path/mod.ts";
+} from "https://deno.land/std@0.183.0/path/mod.ts";
 
 type Params = {
   span: number;
